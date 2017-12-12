c520abd56a4a56ad20fc8b75475c54055f9874cb
yuriyshafranyuk1 <shkafy@gmail.com>
2017-12-12 18:54:38 +0200

    squashing 'release-0.0.1' into 'master'

---
+++ src/main/java/com/shkaff/demogitflow/Demo.java
@@ -0,0 +1,15 @@
+package com.shkaff.demogitflow;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Demo {
+  
+  private String name;
+  
+  private String id;
+  
+  private int version;
+}
