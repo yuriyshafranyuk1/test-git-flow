1bad3a51650c1b73301791823f7b60b9252aa172
yuriyshafranyuk1 <shkafy@gmail.com>
2017-12-12 14:38:19 +0200

    add sample project

---
+++ src/main/java/com/shkaff/demogitflow/DemoGitFlowApplication.java
@@ -0,0 +1,12 @@
+package com.shkaff.demogitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitFlowApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shkaff/demogitflow/DemoGitFlowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shkaff.demogitflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoGitFlowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
